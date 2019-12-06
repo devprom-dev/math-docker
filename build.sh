a36ef15dd70e0eebd694aaba847fa292caacf400
devprom-dev <devprom-dev@users.noreply.github.com>
2019-12-06 13:17:25 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,17 @@ RUN mkdir -p /var/www/devprom && \
   unzip /var/www/devprom/master.zip -d /var/www/devprom/ && \
   mv /var/www/devprom/math-server-master /var/www/devprom/htdocs
 
-RUN apt-get -y install phantomjs 
-
+RUN -get install -qy \ 
+    ca-certificates \
+    bzip2 \
+    curl \
+    libfontconfig \
+    --no-install-recommends \
+    && curl -SL https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 > phantom.tar.bz2 \
+    && bzip2 -d ./phantom.tar.bz2 \
+    && tar -xvf ./phantom.tar -C /usr/local/ --strip-components=1 \
+    && rm phantom.tar
+    
 RUN cd /var/www/devprom/htdocs && \
   apt-get -y install curl && \
   curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && \
